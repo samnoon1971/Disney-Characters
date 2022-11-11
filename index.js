4514790a4b24fc7706fc6afe75cf4b18dff61ecf
samnoon.abrar <samnoonabrar@gmail.com>
2022-11-11 21:23:55 +0600

    api dev

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const fetch = require('node-fetch');
+const express = require("express");
 
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+app.set("view engine", "ejs");
+app.use("./views", require("./controllers/ViewEngines"));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.json());
+app.use(morgan("dev"));
+app.use( express.static( "public" ) );
+app.use(express.static("docs"));
 async function makeRequest() {
     const url = 'https://api.disneyapi.dev/characters';
     const response = await fetch(url);
     const disneyData = await response.json();
-    console.log(disneyData);
 }
-makeRequest();
+makeRequest().then(r =>     console.log(r));
